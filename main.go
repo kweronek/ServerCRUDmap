524ac021d426a8ff0b2f2b7de9a09ddd447887ac
Karsten Weronek <kweronek@fb2.fra-uas.de>
2020-12-17 10:37:46 +0000

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	// connect database
 	persistResource.ConnectDatabase()
 
-	//	next line just generates some test data records:
+	// next line just generates some test data records:
 	modelResource.Init()
 
 	// start webserver
